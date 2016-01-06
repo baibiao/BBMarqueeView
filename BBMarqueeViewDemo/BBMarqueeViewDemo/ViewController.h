a3cd96a8a07ca1773ecf5e4ec6b4297863b7e62b
baibiao <baibiao100@163.com>
2016-01-06 15:29:02 +0800

    Release 1.0.0

---
+++ BBMarqueeViewDemo/BBMarqueeViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BBMarqueeViewDemo
+//
+//  Created by baibiao on 16/1/6.
+//  Copyright © 2016年 baibiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
